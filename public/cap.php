d3d6bd04b18529d2e13dc4b0090dd6fcb4abaecd
eric.chen <eric.chen@dadaabc.com>
2019-11-17 08:24:44 +0800

    ww

--- public/cap.php
+++ public/cap.php
@@ -7,6 +7,10 @@
  */
 session_start();
 header ('Content-Type: image/png');
+header ('Access-Control-Allow-Methods: GET, POST, PUT, DELETE,OPTIONS');
+header ('Access-Control-Allow-Credentials: true');
+header ('Access-Control-Allow-Headers: *');
+header ('Access-Control-Max-Age: 1728000');
 
 $image=imagecreatetruecolor(100, 30);
 //背景颜色为白色
